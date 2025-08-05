bf0bca547e841ac1fdc7fb4ac9dc282453c35df2
DiegoDevEc <diego1503bsc@gmail.com>
2025-08-05 15:19:40 -0500

    feat: add jwt auth and user registration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { PublicController } from './public/public.controller';
+import { PrivateController } from './private/private.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost/complaints-suggestions'),
+    AuthModule,
+    UsersModule,
+  ],
+  controllers: [AppController, PublicController, PrivateController],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
