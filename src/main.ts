ac703a836f9fe40ce63cf3b0774dbca430db852e
Diego Acosta <diego.acosta@externos.bci.cl>
2025-08-21 22:27:14 -0500

    Configuracion cors para front

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,16 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  app.enableCors({
+    origin: ['http://localhost:4200'], // agrega otros orígenes si usas más
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization, X-Requested-With',
+    credentials: false, // si NO usas cookies; deja en false (con JWT en header no hace falta)
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Complaints and Suggestions API')
     .setDescription('API documentation')
